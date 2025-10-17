8ac391fd1fbedc47d2ab5788c4dcaf686a0b701f
ammuneelamprasanth-love <ammuneelamprasanth@gmail.com>
2025-10-17 15:16:23 +0530

    my first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("commit into git first time");
+        System.out.println("upload into gt hub");
+    }
+}
