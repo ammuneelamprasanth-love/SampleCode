fcf8bc1f0213dce404743572e2288fe0fcb225d3
ammuneelamprasanth-love <ammuneelamprasanth@gmail.com>
2025-10-17 16:33:04 +0530

    my second commit

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("commit into git first time");
         System.out.println("upload into gt hub");
+        System.out.println("how are you");
     }
 }
