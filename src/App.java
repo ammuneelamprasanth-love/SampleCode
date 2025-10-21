6756696ebda11a965bcf4823c9021084d1d13b5b
ammuneelamprasanth-love <ammuneelamprasanth@gmail.com>
2025-10-21 12:34:42 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("hello just why");
+        System.out.println("hello my world");
+        System.out.println("Hello, bases");
+        System.out.println("real world");
+        System.out.println("hello there");
+        System.out.println("hello ammu");
+        System.out.println("hello AR");
+        System.out.println("hello Prasanth Neelam");
+        System.out.println("hello neelam how are you");
+
+        System.out.println("i am neelam from main");
+        System.out.println("neelam prasanth reddy sub-branch");
+    }
+}
